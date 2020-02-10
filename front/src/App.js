c3a345a49b0c3d687e7dcaeb16e712cb97cd2776
Ulysse FONTAINE <u.n.fontaine@gmail.com>
2020-02-10 17:20:21 +0100

    minor refactor

--- front/src/App.js
+++ front/src/App.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-import Container from 'react-bootstrap/Container';
-
-import Header from './components/Header'
 import Home from './pages/Home'
 
 const App = () => { return (
-  <Container>
-    <Header />
     <Home />
-  </Container>
 )}
 
 export default App;
--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -2,11 +2,15 @@ import React from 'react';
 import Carous from '../components/Carous';
 import ConferenceListe from '../components/ConferenceListe';
 
+import Container from 'react-bootstrap/Container';
+import Header from '../components/Header'
+
 const Home = () => (
-  <>
+  <Container>
+    <Header />
     <Carous />
     <ConferenceListe />
-  </>
+  </Container>
 )
 
 export default Home
