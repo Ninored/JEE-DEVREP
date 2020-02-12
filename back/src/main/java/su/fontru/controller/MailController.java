89ec7952736a6a54ebc11c6d5eaad52f3ce4c739
kevin <kevintrudu@gmail.com>
2020-02-12 21:48:20 +0100

    upload and download files

--- back/src/main/java/su/fontru/controller/MailController.java
+++ back/src/main/java/su/fontru/controller/MailController.java
@@ -3,6 +3,7 @@ package su.fontru.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import su.fontru.config.MyConstants;
  
+@Service
 @RestController
 public class MailController {
  
---
+++ back/src/main/java/su/fontru/storageFile/FileStorageException.java
@@ -0,0 +1,11 @@
+package su.fontru.storageFile;
+
+public class FileStorageException extends RuntimeException {
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
